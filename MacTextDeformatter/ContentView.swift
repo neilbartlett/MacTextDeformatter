5da45dc02b3ed7c792e2a0909f43d1ce07cad35b
Neil Bartlett <neil.e.bartlett@gmail.com>
2020-06-07 15:34:23 -0400

    Initial Commit

---
+++ MacTextDeformatter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacTextDeformatter
+//
+//  Created by Neil Bartlett on 2020-06-07.
+//  Copyright © 2020 Neil Bartlett. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
